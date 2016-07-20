3aaef4bff000a501ad72775213d97ef83e176992
joffrey <joffrey.armellini@gmail.com>
2016-07-20 19:57:41 -0400

    stable version with list synced by angularfire

--- public/pj.js
+++ public/pj.js
@@ -1,10 +1,14 @@
 //AngularFire - Synchronized Array for Oders
 var app = angular.module("pjApp", ["firebase"]);
+
+//Factory that creates a re-usable order object
+
+
 //Inject firebaseObject into the controller
 app.controller("orderListController", ["$scope", "$firebaseObject", 
 	function($scope, $firebaseObject) {
-	  var ref = firebase.database().ref().child("orders");
+	  var refOrders = firebase.database().ref().child("orders");
 	  //Download firebase into a local object
-	  $scope.orders = $firebaseObject(ref);
+	  $scope.orders = $firebaseObject(refOrders);
 	}
 ]);
